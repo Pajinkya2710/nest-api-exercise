4189692a773acaaa099e3ceb6531af97b5bd75d9
Sandesh Kolhe <sandeshkolhe99@gmail.com>
2023-05-12 18:49:36 +0530

    Intial commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserComment } from 'src/user-comment/entities/user-comment.entity';
 import { User } from 'src/users/entities/user.entity';
-
+//@nestjs/typeorm uses TypeORM because it's the most mature Object Relational Mapper (ORM) available for TypeScript.
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
